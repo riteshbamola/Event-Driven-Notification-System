44adac6f51f34f9ab50485aaccc4f989b25b1e02
Ritesh Bamola <rishubamola01@gmail.com>
2026-02-11 17:36:38 +0530

    Implement Redis Streams worker with message consumption and acknowledgment handling

--- server/index.js
+++ server/index.js
@@ -1,4 +1,81 @@
 import { createClient } from "redis";
+import { randomUUID } from "crypto";
+
+const STREAM_NAME = "notifications-stream";
+const GROUP_NAME = "notifications-group";
+
+async function publishTestEvent(redisClient) {
+  const event = {
+    eventId: randomUUID(),
+    eventType: "USER_REGISTERED",
+    email: "test@example.com",
+    retryCount: "0",
+    createdAt: Date.now().toString(),
+  };
+
+  const id = await redisClient.xAdd(STREAM_NAME, "*", event);
+
+  console.log("📨 Test event published:", id);
+}
+
+async function startWorker(redisClient) {
+  const CONSUMER_NAME = "Worker-1";
+  console.log("Worker Started");
+
+  while (true) {
+    try {
+      const response = await redisClient.xReadGroup(
+        GROUP_NAME,
+        CONSUMER_NAME,
+        {
+          key: STREAM_NAME,
+          id: ">",
+        },
+        {
+          COUNT: 1,
+          BLOCK: 5000,
+        },
+      );
+
+      if (!response) continue;
+
+      for (const stream of response) {
+        for (const message of stream.messages) {
+          const data = message.message;
+          console.log("📥 Processing event:", JSON.stringify(data));
+
+          const success = Math.random() > 0.3; // MIMICKED FAILURE
+
+          if (!success) {
+            throw new Error("Notification failed");
+          }
+
+          await redisClient.xAck(STREAM_NAME, GROUP_NAME, message.id);
+
+          console.log("✅ Message acknowledged:", message.id);
+        }
+      }
+    } catch (error) {
+      console.error("Worker error:", error);
+    }
+  }
+}
+
+async function initStream(redisClient) {
+  try {
+    await redisClient.xGroupCreate(STREAM_NAME, GROUP_NAME, "0", {
+      MKSTREAM: true,
+    });
+
+    console.log("✅ Consumer group created");
+  } catch (err) {
+    if (err.message.includes("BUSYGROUP")) {
+      console.log("ℹ️ Consumer group already exists");
+    } else {
+      throw err;
+    }
+  }
+}
 
 async function startServer() {
   try {
@@ -7,17 +84,25 @@ async function startServer() {
     });
 
     redisClient.on("error", (err) => {
-      console.log("Redis Client Error", err);
+      console.log("Redis Client Error:", err);
     });
 
     redisClient.on("connect", () => {
-      console.log("Connecting to Redis");
+      console.log("Connecting to Redis...");
     });
 
     redisClient.on("ready", () => {
       console.log("✅ Redis Connected");
     });
+
     await redisClient.connect();
+
+    await initStream(redisClient);
+
+    await publishTestEvent(redisClient);
+
+    await startWorker(redisClient);
+
     console.log("🚀 Notification Service Started");
   } catch (error) {
     console.error("❌ Failed to start server:", error);
